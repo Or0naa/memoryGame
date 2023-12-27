6b359572c1966f3bb8814b8f8d4109af09dbdbf0
orna <or0naa@gmail.com>
2023-12-27 18:29:49 +0200

    stars

--- js.js
+++ js.js
@@ -28,7 +28,8 @@ function renderScore() {
     for (let i = 0; i < playersData.length; i++) {
         let playerContainer = document.createElement("div");
         playerContainer.className = "player";
-        playerContainer.innerHTML = playersData[i].name + " score: " + playersData[i].score;
+        playerContainer.innerHTML = playersData[i].name + `
+score: ` + playersData[i].score;
         playersElement.appendChild(playerContainer);
     }
 }
